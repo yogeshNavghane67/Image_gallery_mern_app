be41ddc91681fb25191a602a6f9ff96131590af3
yogeshNavghane67 <yogeshnavghane661@gmail.com>
2023-12-29 01:01:25 +0530

    mongodb setup

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"))
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,9 +15,13 @@ const PORT = process.env.PORT || 5000;
 // });
 
 
-mongoose.connect(process.env.MONGO_URI, () => {
-    console.log("MongoDB connected!");
-}).then((success) => app.listen(5000)).catch((err) => console.log(err.message));
+// mongoose.connect(process.env.MONGO_URI, () => {
+//         console.log("MongoDB connected!");
+//     }).then((success) => app.listen(500,() => {
+//         console.log(`Server started at port: ${PORT}`);
+//     })).catch((err) => console.log(err.message));
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected Successfully")).catch((err) => {console.error(err); });
 
 app.listen(PORT,() => {
     console.log(`Server started at port: ${PORT}`);
