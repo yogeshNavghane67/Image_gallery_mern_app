5156e04207ef21b3d710eae77e0accccfc2b838a
yogeshNavghane67 <yogeshnavghane661@gmail.com>
2023-12-29 00:01:04 +0530

    Mongodb

--- backend/Server.js
+++ backend/Server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose =require("mongoose");
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(process.env.MONGO_URI, () => {
+//     console.log("MongoDB connected!");
+// });
+
+
+mongoose.connect(process.env.MONGO_URI, () => {
+    console.log("MongoDB connected!");
+}).then((success) => app.listen(5000)).catch((err) => console.log(err.message));
 
 app.listen(PORT,() => {
     console.log(`Server started at port: ${PORT}`);
-})
+});
